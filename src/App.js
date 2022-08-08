8eeeaa48d25705a17cb817bc7964de21d0a9ad03
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2022-08-08 10:00:43 -0400

    finish navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import logo from  "./meme.png"
 
 function App() {
   return (
     <div className="main">
       <main className="main-header">
-        
+        <nav className = "navbar">
+        <img className = "logo" src={logo} alt = "logo"/>
+        <h1>Meme Generator</h1>
+        <h3>React Course-Project 3</h3>
+        </nav>
       </main>
     </div>
   );
